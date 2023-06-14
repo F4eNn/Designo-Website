8068899522a81b46605eb2ccb467df73d6231548
Mateusz <mateusz4k@outlook.com>
2023-06-14 13:30:18 +0200

    created how_we_work section

--- src/components/HomeContent/Home.tsx
+++ src/components/HomeContent/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Hero } from './Hero'
 import { Wrapper } from '../UI/Wrapper'
 import { Desings } from './Desings'
+import { AboutUs } from './AboutUs'
 
 export const Home = () => {
 	return (
 		<Wrapper>
 			<Hero />
 			<Desings />
+			<AboutUs />
 		</Wrapper>
 	)
 }
